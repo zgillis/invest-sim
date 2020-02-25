6b5926b7574c8dd9b58b6403e1868ec1708c8135
zgillis <zgillis11@gmail.com>
2020-02-25 15:43:13 -0600

    Investor Simulator (#1)
    * Add Active Profiles
    
    * Add Activity!
    
    * Create .sonarcloud.properties

--- src/main/java/com/zgillis/investsim/controllers/MainController.java
+++ src/main/java/com/zgillis/investsim/controllers/MainController.java
@@ -1,5 +1,6 @@
 package com.zgillis.investsim.controllers;
 
+import com.zgillis.investsim.services.PropertiesConfigLoader;
 import com.zgillis.investsim.util.RendererAssistant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
@@ -16,10 +17,13 @@ public class MainController {
     @Autowired
     RendererAssistant ra;
 
+    @Autowired
+    PropertiesConfigLoader propertiesConfigLoader;
+
     @GetMapping({"/", "/index"})
     public ModelAndView homePage(Model orig) {
         ModelAndView mav = new ModelAndView();
-        orig.addAttribute("title", "Investor Simulator");
+        orig.addAttribute("title", propertiesConfigLoader.getSiteName());
         orig.addAttribute("description", "Allows you to simulate trading.");
         orig.addAttribute("currentts", LocalDateTime.now().toString());
         mav.setViewName("index");
